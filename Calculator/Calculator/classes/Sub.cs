7d7bfb6aa6a32dd5cb82379af3110b83c4883460
you-mine <yu-minei@occ.co.jp>
2019-05-08 11:46:25 +0900

    dev_sub

---
+++ Calculator/Calculator/classes/Sub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.classes
+{
+    class Sub
+    {
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
